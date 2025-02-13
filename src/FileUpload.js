7d7bad933b1689b8b4ec53c68deff4b6b6f570b1
Naimehmet <naimumutozgu@yahoo.com>
2025-02-13 16:36:43 +0000

    Update FileUpload.js

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,1 +1,20 @@
+import React, { useState } from 'react';
+
+function FileUpload() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  return (
+    <div>
+      <h2>Upload a File</h2>
+      <input type="file" onChange={handleFileChange} />
+      {file && <p>Selected file: {file.name}</p>}
+    </div>
+  );
+}
+
+export default FileUpload;
 
