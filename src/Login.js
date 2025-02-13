670d9f51cdecf8ca4e2dcea87bd397a4a63c647b
Naimehmet <naimumutozgu@yahoo.com>
2025-02-13 16:36:19 +0000

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,1 +1,17 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
 
