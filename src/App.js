bc64a66d6cf0a3d7a3b89f27723c5f2482fa0a44
Naimehmet <naimumutozgu@yahoo.com>
2025-02-14 15:10:41 +0000

    Fixed App.js with proper React component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import React from "react";
-import Login from "./Login";
-import FileUpload from "./FileUpload";
-import QnA from "./QnA";
+import "./App.css"; // Make sure this exists
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to My Awesome Website!</h1>
+            <p>This is a test deployment.</p>
+        </div>
+    );
+}
+
+export default App;
