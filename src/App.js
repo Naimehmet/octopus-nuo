b719a712737ba056bb5aebf8946885adc1b2d22a
Naimehmet <naimumutozgu@yahoo.com>
2025-02-16 13:13:11 +0000

    Fixed App.js component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
-import "./App.css"; // Make sure this exists
+import "./App.css"; // Make sure this exists!
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Welcome to My Awesome Website!</h1>
-            <p>This is a test deployment.</p>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Welcome to My Awesome AI!</h1>
+    </div>
+  );
 }
 
 export default App;
