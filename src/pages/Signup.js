1d9361ddfe1dd3504ae128eb66db0b379dcd250a
Naimehmet <naimumutozgu@yahoo.com>
2025-02-17 12:28:17 +0000

    Added missing pages folder and components

---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <h2>Signup Page</h2>
+      <p>Create an account to continue.</p>
+    </div>
+  );
+}
+
+export default Signup;
